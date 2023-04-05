764c28dc55afc0c592293b064bcc474dd753d228
massa <zoumaniguimassa12@gmail.com>
2023-04-05 22:11:21 +0000

    Update and adding to my js file tasks.sort((a, b) => a.index- b.index) to sort my array by index

--- src/index.js
+++ src/index.js
@@ -2,34 +2,17 @@ import _ from 'lodash';
 import './style.css';
 
 const tasks = [
-  { description: "Clean the house", completed: false, index: 0 },
-  { description: "Buy groceries", completed: true, index: 1 },
+  { description: "Clean the house", completed: false, index: 1 },
+  { description: "Buy groceries", completed: true, index: 3 },
   { description: "Pay bills", completed: false, index: 2 },
-  { description: "Go for a run", completed: true, index: 3 },
+  { description: "Go for a run", completed: true, index: 0 },
   { description: "Call mom", completed: false, index: 4 }
 ];
 
-// console.log(tasks);
-
-// function renderTasks() {
-//   const taskList = document.getElementById("task-list");
-//   const container =document.querySelector('.container')
-//   // Clear any existing tasks
-//   taskList.innerHTML = "";
-  
-//   // Render each task as a list item
-//   tasks.forEach(task => {
-//     const listItem = document.createElement("li");
-//     listItem.innerHTML =`<div class="divli"><div><input type="checkbox" id="my-checkbox" name="my-checkbox" />
-//     <label for="demoCheckbox"> ${task.description}</div>  <i class="fas fa-ellipsis-v" id='fas'></i>
-//     </div>`  
-//     taskList.appendChild(listItem);
-//   });
-//   container.style.diplay='block';
-// }
 const renderTasks = () => {
   const taskList = document.getElementById("task-list");
   const container = document.querySelector('.container');
+  tasks.sort((a, b) => a.index- b.index);
 
   // Clear any existing tasks
   taskList.innerHTML = "";
